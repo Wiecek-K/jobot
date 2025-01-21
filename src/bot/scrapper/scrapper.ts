7f3bf01673a23f40d3b6e69bbda1f1a5e58812ae
Wiecek-K <krystiandwiecek@gmail.com>
2025-01-21 23:10:06 +0100

    feat: save scraped results to csv file

--- src/bot/scrapper/scrapper.ts
+++ src/bot/scrapper/scrapper.ts
@@ -52,7 +52,7 @@ export class BrowserManager {
 
   public async init(): Promise<void> {
     this.browser = await puppeteer.launch({
-      headless: true,
+      headless: false,
       args: ['--no-sandbox', '--disable-gpu', '--mute-audio'],
     })
     console.log('Browser initialized!')
