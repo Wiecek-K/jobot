f3afa5ba1c5d99079067d551b18dc208a05c9dd8
Wiecek-K <krystiandwiecek@gmail.com>
2025-01-24 00:17:12 +0100

    refactor: remove unnecessary comments

--- src/server.ts
+++ src/server.ts
@@ -70,34 +70,3 @@ schedule.scheduleJob('0 9 * * 1-5', () => {
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`)
 })
-
-// import { createServer, Server, ServerResponse } from 'http'
-// import schedule from 'node-schedule'
-
-// const server: Server = createServer((request, response: ServerResponse) => {
-//   response.writeHead(200, { 'Content-Type': 'text/plain' })
-//   response.end('Hello World!\n')
-// })
-
-// schedule.scheduleJob('0 9 * * 1-5', () => {
-//   console.log('Running scheduled job: run-cron-job')
-
-//   exec(
-//     'pnpm scrap:offers -s "Javascript Developer" -l 5 -t 30',
-//     (error, stdout, stderr) => {
-//       if (error) {
-//         console.error(`Error executing script: ${error.message}`)
-//         return
-//       }
-//       if (stderr) {
-//         console.error(`Script error output: ${stderr}`)
-//         return
-//       }
-//       console.log(`Script output: ${stdout}`)
-//     }
-//   )
-// })
-
-// server.listen(PORT, () => {
-//   console.log(`Server running at http://localhost:${PORT}/`)
-// })
