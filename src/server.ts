483329313a4e1c0d9cecb205e83651da91055501
Wiecek-K <krystiandwiecek@gmail.com>
2025-01-24 00:15:01 +0100

    feat: created endpoints for fetch cached offers

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,34 @@ import express, { Request, Response } from 'express'
 import { exec } from 'child_process'
 import { findOffers } from './scripts/handleEndpoint'
 import schedule from 'node-schedule'
+import fs from 'fs'
+import path from 'path'
 
 const app = express()
 const PORT = process.env.PORT || 4200
+
+app.get('/scrap-results/justjoinit', async (req: Request, res: Response) => {
+  const filePath = path.join(__dirname, '../scrap-results/JustJoinIt.json')
+  console.log(filePath)
+
+  try {
+    const fileData = await fs.promises.readFile(filePath, 'utf-8')
+    res.status(200).json(JSON.parse(fileData))
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to read the file' })
+  }
+})
+
+app.get('/scrap-results/pracujpl', async (req: Request, res: Response) => {
+  const filePath = path.join(__dirname, '../scrap-results/PracujPl.json')
+
+  try {
+    const fileData = await fs.promises.readFile(filePath, 'utf-8')
+    res.status(200).json(JSON.parse(fileData))
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to read the file' })
+  }
+})
 
 app.get('/offers/:searchValue', async (req: Request, res: Response) => {
   const searchValue = req.params.searchValue
